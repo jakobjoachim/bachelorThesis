ed01fddd9075be60a7aa8c83de9c6153a205e37f
Jakob Joachim <jak.joachim@gmail.com>
2017-05-14 15:09:50 +0200

    latex can now be compiled

---
+++ latex/runLatex.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+function clear {
+  for i in aux idx lof log lot nlo synctex.gz toc blg bbl dvi
+  do
+    rm -f thesis.$i
+  done
+}
+
+function copyResourcesToOut {
+  cp ../*.sty .
+  cp ../thesis.bib .
+  cp -r ../logo .
+  cp -r ../images/*.png .
+  cp ../*.tex .
+}
+
+function texify {
+  pdflatex -interaction=batchmode thesis.tex
+}
+
+function compile {
+  texify
+  bibtex thesis.aux
+  # makeglossaries thesis
+  texify
+  texify
+}
+
+cd "out" && copyResourcesToOut && clear && compile && open thesis.pdf
