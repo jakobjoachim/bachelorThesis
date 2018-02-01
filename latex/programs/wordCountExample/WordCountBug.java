8cdd350d3af3c71aaf7443bccf3360044c0d73cf
jakobjoachim <mail@jakobjoachim.com>
2018-02-02 00:28:36 +0100

    laueft

--- latex/programs/wordCountExample/WordCountBug.java
+++ latex/programs/wordCountExample/WordCountBug.java
@@ -6,13 +6,15 @@ public class WordCount {
         Map<String, Integer> result = new HashMap<>();
         String[] words = args[0].split("\\s+");
         int numberOfWords = words.length;
-        System.out.println("There are " + numberOfWords + " words in total.");
+        System.out.println("There are " + numberOfWords
+        + " words in total.");
         for (String word : words) {
             int count = result.getOrDefault(word, 1);
             result.put(word.toLowerCase(), count + 1);
         }
         int uniqueWords = result.size();
-        System.out.println("Of these " + numberOfWords + " words " + uniqueWords + " are unique.");
+        System.out.println("Of these " + numberOfWords + " words "
+         + uniqueWords + " are unique.");
         System.out.println("The unique words are: ");
         for (String word : result.keySet()) {
             System.out.println(word + ": " + result.get(word));
